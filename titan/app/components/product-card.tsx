ba3cd91a2ced467e161f9fd58c0e6db2ff074306
MaksimusK <maksimusstalker2003@gmail.com>
2024-09-29 19:04:05 +0700

    Добавление навигации

---
+++ titan/app/components/product-card.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AddToCart from './add-to-cart'
+
+const ProductCard  = () => {
+  return (
+    <div>
+       <AddToCart />
+    </div>
+  )
+}
+
+export default ProductCard
