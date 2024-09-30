e74f46b3d5f90758e5521853a52a00fc95ae6019
MaksimusK <maksimusstalker2003@gmail.com>
2024-09-30 15:07:41 +0700

    Добавление таблицы с данными(исправлено)

--- titan/app/page.tsx
+++ titan/app/page.tsx
@@ -19,6 +19,7 @@ export default async function Home() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const users: User[] = await res.json();
   return (
+
     <main>
       
         <h1 className={styles.Title}>Добро пожаловать на сайт!</h1>
@@ -34,35 +35,53 @@ export default async function Home() {
       </div>
           
       <Link href="/users">Users</Link>
+      
 
       <Table>
-      <TableHeader>
-        <TableRow>
-          {/* <TableHead className="w-[100px]">Invoice</TableHead> */}
-          <TableHead>Заголовок задачи</TableHead>
+  <TableHeader>
+    <TableRow>
+      <TableHead>Заголовок задачи</TableHead>
+      <TableHead>Действия</TableHead>
+    </TableRow>
+  </TableHeader>
+  <TableBody>
+    {users.map(user => (
+      <TableRow key={user.id}>
+        <TableCell>{user.name}</TableCell>
+        <TableCell>
           <button>Редактировать</button>
-          {/* <TableHead>Method</TableHead>
-          <TableHead className="text-right">Amount</TableHead> */}
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <TableRow>
-          {/* <TableCell className="font-medium">INV001</TableCell>
-          <TableCell>Paid</TableCell>
-          <TableCell>Credit Card</TableCell>
-          <TableCell className="text-right">$250.00</TableCell> */}
-          
-          <TableCell style={{width: '30%'}}>
-             {users.map(user => <TableRow key={user.id}>{user.name}</TableRow>)}
-          </TableCell>
-          
-        </TableRow>
-      </TableBody>
-    </Table>
+        </TableCell>
+      </TableRow>
+    ))}
+  </TableBody>
+</Table>
 
+    {/* <Table>
+  <TableCaption>A list of your recent invoices.</TableCaption>
+  <TableHeader>
+    <TableRow>
+      <TableHead className="w-[100px]">Invoice</TableHead>
+      <TableHead>Status</TableHead>
+      <TableHead>Method</TableHead>
+      <TableHead className="text-right">Amount</TableHead>
+    </TableRow>
+  </TableHeader>
+  <TableBody>
+    <TableRow>
+      <TableCell className="font-medium">INV001</TableCell>
+      <TableCell>Paid</TableCell>
+      <TableCell>Credit Card</TableCell>
+      <TableCell className="text-right">$250.00</TableCell>
+    </TableRow>
+  </TableBody>
+</Table> */}
     </main>
   );
+  
+  
+  
 }
+
 
  
  
