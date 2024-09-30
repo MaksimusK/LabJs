86fd9df2211abc4e125cc85caa25345ead283918
MaksimusK <maksimusstalker2003@gmail.com>
2024-09-30 13:44:56 +0700

    Добавление таблицы

--- titan/app/page.tsx
+++ titan/app/page.tsx
@@ -1,14 +1,25 @@
+
 import Image from "next/image";
 import Link from "next/link";
 import styles from './Page.module.css'
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 export default function Home() {
   return (
     <main>
+      
         <h1 className={styles.Title}>Добро пожаловать на сайт!</h1>
       
       <div className={styles.block}>
         <ul >
-        <li><Link href="/createTask">Создать задачу</Link></li> 
+        <li><button className={styles.Button} ><Link href="/createTask">Создать задачу</Link></button></li>
         <li><button className={styles.Button} ><Link href="/login">Войти</Link></button></li>
         <li><button className={styles.Button} ><Link href="/avatar">Аватар</Link></button></li>
         <li><button className={styles.Button} ><Link href="/exit">Выйти</Link></button></li>
@@ -20,3 +31,6 @@ export default function Home() {
     </main>
   );
 }
+
+ 
+ 
